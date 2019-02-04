29a69490d34626fb430d712a8fb19b92140fc35c
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-02-04 09:18:50 -0700

    Update normal.py

--- SmallSanto - Distributed/src/configs/normal.py
+++ SmallSanto - Distributed/src/configs/normal.py
@@ -34,7 +34,7 @@ class PlayConfig:
 class TrainerConfig:
     def __init__(self):
         self.batch_size = 1024  # 2048
-        self.epoch_to_checkpoint = 8
+        self.epoch_to_checkpoint = 80
         self.start_total_steps = 0
         self.save_model_steps = 9999 # 150
         self.load_data_steps = 9999 # 150
