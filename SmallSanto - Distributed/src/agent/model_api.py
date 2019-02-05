7565bd890e1459afa7feb542a35ecdd823446345
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-02-05 11:18:10 -0700

    Update model_api.py

--- SmallSanto - Distributed/src/agent/model_api.py
+++ SmallSanto - Distributed/src/agent/model_api.py
@@ -9,10 +9,10 @@ class ModelAPI:
         assert x.ndim in (3, 4)
 
         # Changed inital 2,3,3 to 8,3,3.  2->8 is the number of initial feature map
-        assert x.shape == (2, 3, 3) or x.shape[1:] == (2, 3, 3)
+        assert x.shape == (4, 3, 3) or x.shape[1:] == (4, 3, 3)
         orig_x = x
         if x.ndim == 3:
-            x = x.reshape(1, 2, 3, 3)
+            x = x.reshape(1, 4, 3, 3)
 
         policy, value = self.agent_model.model.predict_on_batch(x)
 
